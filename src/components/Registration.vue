ddc8205869ce595b2e88a396752e845a765dabc9
Артур Галеев <a.galeev@tapir.ws>
2020-10-31 02:39:34 +0500

    add inputs

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -2,6 +2,27 @@
   <div class="registartion">
     <h1 class="registartion__title">Авто за 3000 ₽</h1>
     <h2 class="registartion__subtitle">Регистрация чека</h2>
+    <div class="wrp-field">
+      <label>Дата покупки*</label>
+      <div class="wrp-input">
+        <input type="text" placeholder="" />
+        <div class="wrp-field__icon">
+          <svg class="icon-date">
+            <use xlink:href="/images/svg/sprite.svg#icon-svg-date"></use>
+          </svg>
+        </div>
+      </div>
+      <!-- <p class="wrp-field__error">
+        Дата покупки не соответствует условиям акции.
+      </p> -->
+    </div>
+    <div class="wrp-field">
+      <label>Сумма покупки*</label>
+      <div class="wrp-input">
+        <input type="text" placeholder="" />
+      </div>
+    </div>
+    <button class="btn btn-primary">Зарегистрировать купон</button>
   </div>
 </template>
 
